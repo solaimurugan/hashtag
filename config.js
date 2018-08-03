d9370fd09bd498638d8421a26da4f20255f36484
solaimurugan v <solaimurugan.v@gmail.com>
2018-08-04 02:00:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
 const twitterApp = {
+  access_token: process.env.ACCESS_TOKEN,
+   
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   //timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
 };
 
 module.exports = {
   twitterApp,
-  userName: process.env.USERNAME
-  dbUserName: process.env.DB_USERNAME
-  dbUserPwd: process.env.DB_USERPWD	
+  userName: process.env.USERNAME,
+  dbUserName: process.env.DB_USERNAME,
+  dbUserPwd: process.env.DB_USERPWD	,
    botToken:process.env.BOT_TOKEN
 };
