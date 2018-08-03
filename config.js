93ca1b18f2f73c1792e06a963e259ed2294fd761
solaimurugan v <solaimurugan.v@gmail.com>
2018-08-04 02:02:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const twitterApp = {
   access_token: process.env.ACCESS_TOKEN,
-   
-  consumer_key: process.env.CONSUMER_KEY,
+    consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   //timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
